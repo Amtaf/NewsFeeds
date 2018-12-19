72fe772a6d84137af6809412bd936d77fa0cff56
Amtaf <abdulqadirfatma@gmail.com>
2018-12-19 15:22:19 +0300

    NewsApp fetches RSS feeds from news Sites

--- NewsApp.py
+++ NewsApp.py
@@ -1,7 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
-
+from flask import request
 app = Flask(__name__)
 RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -11,7 +11,12 @@ RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route('/')
 @app.route('/<publication>')
-def get_news(publication='bbc'):
+def get_news():
+    query = request.form.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = 'bbc'
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
     return render_template('home.html', article=feed['entries'])
 
